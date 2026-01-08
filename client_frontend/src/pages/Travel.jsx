5a85e3957fc08397a11d93599bf32f5e9fbeff5f
Uthasra <uthsara12087@usci.ruh.ac.lk>
2026-01-08 23:44:45 +0530

    Travel page updating
    travel

---
+++ client_frontend/src/pages/Travel.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Navbar from '../components/Navbar';
+import Footer from '../components/Footer';
+import EllaTravelGuide from '../components/EllaTravelGuide';
+
+const Travel = () => {
+  return (
+    <div>
+        <Navbar />
+            <EllaTravelGuide />
+        
+    </div>
+  )
+}
+
+export default Travel;
+
